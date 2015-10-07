0d3e8cc2b951f1ffc609446063b4c3f63ff0894c
Rostepher <ross.m.bayer@gmail.com>
2015-10-07 11:31:37 -0500

    Added tests to the webhooks service.

--- pagerduty/webhooks.go
+++ pagerduty/webhooks.go
@@ -2,7 +2,7 @@ package pagerduty
 
 import (
 	"encoding/json"
-	"net/http"
+	"io"
 	"time"
 )
 
@@ -22,20 +22,39 @@ type WebhooksService struct {
 	client *Client
 }
 
+type WebhookIncident struct {
+	ID                    *string                `json:"id"`
+	Number                *int                   `json:"incident_number"`
+	CreatedOn             *time.Time             `json:"created_on"`
+	Status                *string                `json:"status"`
+	URL                   *string                `json:"html_url"`
+	Key                   *string                `json:"incident_key"`
+	Service               *Service               `json:"service"`
+	AssignedToUser        *User                  `json:"assigned_to_user"`
+	TriggerSummaryData    map[string]interface{} `json:"trigger_summary_data"`
+	TriggerDetailsHTMLURL *string                `json:"trigger_details_html_url"`
+	LastStatusChangeOn    *time.Time             `json:"last_status_change_on"`
+	LastStatusChangeBy    *string                `json:"last_status_change_by"`
+}
+
+type webhookIncidentWrapper struct {
+	Incident *WebhookIncident `json:"incident"`
+}
+
 type WebhookMessage struct {
-	ID        *int       `json:"id,omitempty"`
-	Type      *string    `json:"type,omitempty"`
-	CreatedOn *time.Time `json:"created_on,omitempty"`
-	Data      []Incident `json:"data,omitempty"`
+	ID        *string                 `json:"id"`
+	Type      *string                 `json:"type"`
+	CreatedOn *time.Time              `json:"created_on"`
+	Data      *webhookIncidentWrapper `json:"data"`
 }
 
 type webhookMessageListWrapper struct {
 	Messages []WebhookMessage `json:"messages"`
 }
 
-func (s *WebhooksService) DecodeMessages(resp *http.Response) ([]WebhookMessage, error) {
+func (s *WebhooksService) DecodeMessages(reader io.Reader) ([]WebhookMessage, error) {
 	messages := new(webhookMessageListWrapper)
-	err := json.NewDecoder(resp.Body).Decode(messages)
+	err := json.NewDecoder(reader).Decode(messages)
 	if err != nil {
 		return nil, err
 	}
---
+++ pagerduty/webhooks_test.go
@@ -0,0 +1,86 @@
+package pagerduty_test
+
+import (
+	. "github.com/hudl/go-pagerduty/pagerduty"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	"encoding/json"
+	"io"
+	"strings"
+)
+
+const (
+	webhookJSON = `{
+		"messages": [{
+			"id": "bb8b8fe0-e8d5-11e2-9c1e-22000afd16cf",
+			"created_on": "2013-07-09T20:25:44Z",
+			"type": "incident.trigger",
+			"data": {
+				"incident": {
+					"id": "PIJ90N7",
+					"incident_number": 1,
+					"created_on": "2013-07-09T20:25:44Z",
+					"status": "triggered",
+					"html_url": "https://acme.pagerduty.com/incidents/PIJ90N7",
+					"incident_key": "null",
+					"service": {
+						"id": "PBAZLIU",
+						"name": "service",
+						"html_url": "https://acme.pagerduty.com/services/PBAZLIU"
+					},
+					"assigned_to_user": {
+						"id": "PPI9KUT",
+						"name": "Alan Kay",
+						"email": "alan@pagerduty.com",
+						"html_url": "https://acme.pagerduty.com/users/PPI9KUT"
+					},
+					"trigger_summary_data": {
+					"subject": "45645"
+					},
+					"trigger_details_html_url": "https://acme.pagerduty.com/incidents/PIJ90N7/log_entries/PIJ90N7",
+					"last_status_change_on": "2013-07-09T20:25:44Z",
+					"last_status_change_by": "null"
+				}
+			}
+		}]
+	}`
+)
+
+type webhookMessageListWrapper struct {
+	Messages []WebhookMessage `json:"messages"`
+}
+
+var _ = Describe("Webhooks", func() {
+	var expectedMessages webhookMessageListWrapper
+	json.Unmarshal([]byte(webhookJSON), &expectedMessages)
+
+	Describe("DecodeMessages", func() {
+		Context("with a valid, non-empty webhook reader", func() {
+			var (
+				env      *TestEnvironment
+				reader   io.Reader
+				messages []WebhookMessage
+				err      error
+			)
+
+			BeforeEach(func() {
+				env = NewTestEnvironment()
+				reader = strings.NewReader(webhookJSON)
+				messages, err = env.Client.Webhooks.DecodeMessages(reader)
+			})
+
+			AfterEach(func() { env.Server.Close() })
+
+			It("should not return an error", func() {
+				Expect(err).NotTo(HaveOccurred())
+			})
+
+			It("should return the expected messages", func() {
+				Expect(messages).NotTo(BeEmpty())
+				Expect(messages).To(Equal(expectedMessages.Messages))
+			})
+		})
+	})
+})
